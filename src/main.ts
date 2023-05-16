85e55d3a2b3f83b4e643420125758f47081955e7
“zygalaxy” <694553456@qq.com>
2023-05-16 21:35:02 +0800

    feat: 初始化项目结构

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import ElementPlus from "element-plus"
+import 'element-plus/dist/index.css'
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.min.mjs'
+
+const app = createApp(App)
+
+app.use(ElementPlus,{locale:zhCn})
+app.mount('#app')
